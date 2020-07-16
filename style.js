e5f5bdcb7bb507873ac78af409f27e8ee0bf5d43
DixonKimandu <38883485+DixonKimandu@users.noreply.github.com>
2020-07-16 13:51:37 +0300

    Update style.js

--- style.js
+++ style.js
@@ -21,3 +21,13 @@ $(window).on('scroll', function(){
   }
 })
 /*Navbar End*/
+/*Navbar Start*/
+function mobileNavbar() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+/*Navbar End*/
